603b9850bdb53a3bf295345e568f4ac47588d633
Jonathan <mcjhona069@gmail.com>
2022-07-12 23:09:18 -0500

    Vistas de la aplicacion

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Página principal página Platzi App")
+
+
+def detail(request, question_id):
+    return HttpResponse("Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse("Estás viendo los resultados de pregunta número {question_id}")
+
+
+def detail(request, question_id):
+    return HttpResponse("Estás votando la pregunta número {question_id}")

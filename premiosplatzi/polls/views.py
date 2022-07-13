a4e249b916d7f3245846c3c2968c50c2ad95a75d
Jonathan <mcjhona069@gmail.com>
2022-07-12 23:50:13 -0500

    Vista vote

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -1,7 +1,8 @@
+from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.all()
@@ -21,5 +22,16 @@ def results(request, question_id):
     return HttpResponse("Estás viendo los resultados de pregunta número {question_id}")
 
 
-def detail(request, question_id):
-    return HttpResponse("Estás votando la pregunta número {question_id}")
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question":question,
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))

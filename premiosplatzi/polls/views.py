fdc3367933c4c3d8fd78b24ee41f5da665c77fdf
Jonathan <mcjhona069@gmail.com>
2022-07-12 23:52:57 -0500

    Vista results

--- premiosplatzi/polls/views.py
+++ premiosplatzi/polls/views.py
@@ -19,8 +19,10 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse("Estás viendo los resultados de pregunta número {question_id}")
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question":question
+    })
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)

4d1561c551a0274adc8eb3079101d951f2e3d298
Jonathan <mcjhona069@gmail.com>
2022-07-14 21:51:50 -0500

    Mejorando el Admin - Change List

--- premiosplatzi/polls/admin.py
+++ premiosplatzi/polls/admin.py
@@ -7,7 +7,10 @@ class ChoiceInline(admin.StackedInline):
     extra: 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'questiontext']
+    fields = ['pub_date', 'question_text']
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
